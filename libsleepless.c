becfd03fa557de839d7a108433718b22bc971fe9
Emanuele Aina <em@nerd.ocracy.org>
2007-05-08 17:39:16 +0200

    Removed a comment obsoleted by the README

--- libsleepless.c
+++ libsleepless.c
@@ -4,8 +4,6 @@
 #include <dlfcn.h>
 #include <time.h>
 
-/* Example taken from Gilad Ben-Yossef http://benyossef.com/ */
-
 unsigned int sleep(unsigned int seconds)
 {
     unsigned int (*origsleep)(unsigned int);
